bee574a379ae40b44811408125ca4e7f2c572b30
Robley Gori <robleyadrian@gmail.com>
2017-07-08 20:03:19 +0300

    [NEW] Add component for bucketlist items

---
+++ src/app/bucket/bucket.component.ts
@@ -0,0 +1,35 @@
+import { Component, EventEmitter, Input, Output, OnInit } from '@angular/core';
+import { BucketList } from '../bucketlist/bucket-list';
+
+@Component({
+  selector: 'app-bucket',
+  templateUrl: './bucket.component.html',
+  styleUrls: ['./bucket.component.css']
+})
+export class BucketComponent implements OnInit {
+
+        @Input()
+        bucketlist: BucketList;
+
+        @Output()
+        update: EventEmitter<BucketList> = new EventEmitter();
+
+        @Output()
+        remove: EventEmitter<BucketList> = new EventEmitter();
+
+        constructor() {
+        }
+
+        updateBucketList(bucketlist: BucketList) {
+          this.update.emit(bucketlist);
+        }
+
+        removeBucketList(bucketlist: BucketList) {
+          //this.bucketListService.deleteBucketListById(bucketlist.id);
+          this.remove.emit(bucketlist);
+        }
+
+        ngOnInit() {
+        }
+
+}
