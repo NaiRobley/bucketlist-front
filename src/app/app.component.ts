8f757b485fc7dc377c537de809d2aec8b7f5280d
Robley Gori <robleyadrian@gmail.com>
2017-07-08 19:57:30 +0300

    [UPDATE] Update component imports, implement service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,42 +1,78 @@
-import { Component } from '@angular/core';
-import { BucketList } from './bucket-list';
-import { BucketListService } from './bucket-list.service';
+import { Component, OnInit } from '@angular/core';
+import { BucketList } from './bucketlist/bucket-list';
+import { BucketListService } from './bucketlist/bucket-list.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: []
+  providers: [BucketListService]
 })
 
 
 
 
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Your Bucket Lists';
 
   newBucketList: BucketList = new BucketList();
 
+  bucketlists: BucketList[] = [];
+
   constructor(private bucketListService: BucketListService) { }
+
+  public ngOnInit() {
+    this.title = 'Your Bucket Lists';
+    this.bucketListService
+      .getAllBucketLists()
+      .subscribe(
+        (bucketlists) => {
+          this.bucketlists = bucketlists;
+        }
+      );
+  }
 
   // handle event emitted by component
   onAddBucketList(bucketlist: BucketList){
-    this.bucketListService.addBucketList(bucketlist);
+    this.bucketListService
+    .addBucketList(bucketlist)
+    .subscribe(
+      (newBucketList) => {
+        this.bucketlists = this.bucketlists.concat(newBucketList);
+      }
+    );
   }
 
   // Remove a bucket list
   onRemoveBucketList(bucketlist) {
-    this.bucketListService.deleteBucketListById(bucketlist.id);
+    this.bucketListService
+    .deleteBucketListById(bucketlist.id)
+    .subscribe(
+      (_) => {
+        this.bucketlists = this.bucketlists.filter((t) => t.id !== bucketlist.id);
+      }
+    );
   }
+  //
+  // // Update a bucket list
+  // onUpdateBucketList(bucketlist) {
+  //   this.bucketListService
+  //   .updateBucketList(bucketlist.id)
+  //   .subscribe(
+  //     (bucketlist) => {
+  //       this.bucketlists = this.bucketlists.filter((t) => t.id === bucketlist.id);
+  //     }
+  //   );
+  // }
 
   // Get all bucket lists
-  get bucketlists(){
-    return this.bucketListService.getAllBucketLists();
-  }
+  // get bucketlists(){
+  //   return this.bucketListService.getAllBucketLists();
+  // }
 
   // Redirect to the home page of bucket lists
-  bucketlistHome(){
-
-  }
+  // bucketlistHome(){
+  //
+  // }
 
 }
