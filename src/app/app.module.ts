06260439a14d3152ae39a736afea6abfc0163b4e
Robley Gori <robleyadrian@gmail.com>
2017-07-08 19:58:07 +0300

    [NEW] Add new components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
 
 import { FormsModule } from '@angular/forms';
 
-import { BucketListService } from './bucket-list.service';
+import { BucketListService } from './bucketlist/bucket-list.service';
 
 import { AppComponent } from './app.component';
-import { BucketlistListHeaderComponent } from './bucketlist-list-header/bucketlist-list-header.component';
-import { BucketlistLoginComponent } from './bucketlist-login/bucketlist-login.component';
-import { BucketlistRegisterComponent } from './bucketlist-register/bucketlist-register.component';
-import { BucketlistHomeComponent } from './bucketlist-home/bucketlist-home.component';
-import { BucketlistAccountComponent } from './bucketlist-account/bucketlist-account.component';
-import { BucketlistSingleComponent } from './bucketlist-single/bucketlist-single.component';
-import { BucketlistFooterComponent } from './bucketlist-footer/bucketlist-footer.component';
+import { ApiService } from './api.service';
+import { BucketlistComponent } from './bucketlist/bucketlist.component';
+import { BucketComponent } from './bucket/bucket.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
 
 @NgModule({
   imports: [
     BrowserModule,
-    FormsModule ],
+    FormsModule,
+    HttpModule ],
   declarations: [
     AppComponent,
-    BucketlistListHeaderComponent,
-    BucketlistLoginComponent,
-    BucketlistRegisterComponent,
-    BucketlistHomeComponent,
-    BucketlistAccountComponent,
-    BucketlistSingleComponent,
-    BucketlistFooterComponent
+    BucketlistComponent,
+    BucketComponent,
+    HomeComponent,
+    AuthComponent
   ],
-  providers: [BucketListService],
+  providers: [BucketListService, ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
