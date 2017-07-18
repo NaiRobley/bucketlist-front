598a0c56a74681b48b863bcf9c4d95eaba0caaf6
Robley Gori <robleyadrian@gmail.com>
2017-07-18 21:51:36 +0300

    [UPDATE] Remove unused imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,12 @@ import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RouterModule, Routes } from '@angular/router';
 import { FormsModule } from '@angular/forms';
-import { NglModule } from 'ng-lightning/ng-lightning';
-import 'hammerjs';
 
 import { BucketListService } from './bucketlist/bucket-list.service';
 
 import { AppComponent } from './app.component';
 import { ApiService } from './api.service';
 import { BucketlistComponent } from './bucketlist/bucketlist.component';
-import { BucketComponent } from './bucket/bucket.component';
 import { HomeComponent } from './home/home.component';
 import { AuthComponent } from './auth/auth.component';
 import { ItemComponent } from './item/item.component';
@@ -31,7 +28,6 @@ import { AppRoutingModule, routingComponents } from './app.routes';
   declarations: [
     AppComponent,
     BucketlistComponent,
-    BucketComponent,
     HomeComponent,
     AuthComponent,
     routingComponents,
