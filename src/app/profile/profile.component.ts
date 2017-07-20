920ee82b4743aabe87d04546b0b8269d892857de
Robley Gori <robleyadrian@gmail.com>
2017-07-20 07:40:19 +0300

    [NEW] Add profile management component

---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
+import { User } from '../auth/user';
+
+declare var Materialize: any;
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.css']
+})
+export class ProfileComponent implements OnInit {
+
+  username: string = '';
+
+  email: string = '';
+
+  constructor(
+    private apiService: ApiService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    if (localStorage.getItem('login_status') == '0') {
+      this.router.navigate(['/auth']);
+    } else {
+      this.username = localStorage.getItem("current_user");
+      this.email = localStorage.getItem("email");
+    }
+  }
+
+  updateUsername(username, password, newUserName) {
+    this.apiService
+      .updateUsername(username, password, newUserName)
+      .subscribe(response => {
+        localStorage.setItem('current_user', response.json()['username']);
+        this.username = localStorage.getItem("current_user");
+        Materialize.toast(response.json()['message'], 4000);
+      });
+  }
+
+  updateEmail(username, password, newEmail) {
+    this.apiService
+      .updateEmail(username, password, newEmail)
+      .subscribe(response => {
+        localStorage.setItem('email', response.json()['email']);
+        this.email = localStorage.getItem("email");
+        Materialize.toast(response.json()['message'], 4000);
+      });
+  }
+
+  updatePassword(username, password, newPassword) {
+    this.apiService
+      .updatePassword(username, password, newPassword)
+      .subscribe(response => {
+        Materialize.toast(response.json()['message']), 4000;
+      });
+  }
+
+}
